7ee26e5cb68b139c129e4eef81aabcf221bd34dd
TedCCLeung <89104258+TedCCLeung@users.noreply.github.com>
2022-05-19 23:59:30 +0100

    Initial commit

--- R/utils.R
+++ R/utils.R
@@ -148,3 +148,15 @@ read_homer_output <- function(
   return(df_out)
 }
 
+
+input_homer <- function(
+  dir
+){
+  motif_files <- list.files(path = dir, recursive = TRUE, pattern = "knownResults.txt", all.files = TRUE, full.names = TRUE)
+  cond_names <- motif_files %>% strsplit(split = "/") %>% sapply(function(x){magrittr::extract2(x, length(x)-1)})
+
+  df_result <- Reduce(rbind, Map(read_homer_output, motif_files, cond_names))
+
+  return(df_result)
+}
+
